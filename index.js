334e639f533ed6f679337e2d147cb65b00ee31cc
Lucas-Rock-Clear <lucas.rock.clear@gmail.com>
2023-04-12 23:49:21 -0300

    Basic configuration, dependencies and homepage

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-/** @format */
-
+import 'react-native-gesture-handler';
+import React from 'react';
 import {AppRegistry} from 'react-native';
-import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+import App from './src/App'
+import { FeedProvider } from "./src/data/contexts/FeedContext"
+import { UserProvider } from "./src/data/contexts/UserContext"
+import { EventProvider } from "./src/data/contexts/EventContext"
+
+import axios from 'axios'
+
+axios.defaults.baseURL='https://instaclone-b78e8-default-rtdb.firebaseio.com/'
+
+const Root = () => (
+    <EventProvider>
+        <UserProvider>
+            <FeedProvider>
+                <App/>
+            </FeedProvider>
+        </UserProvider>
+    </EventProvider>
+)
+
+AppRegistry.registerComponent(appName, () => Root);
