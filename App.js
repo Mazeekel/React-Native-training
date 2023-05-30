305f2aefd91ef562cb3b484177323109efced5c5
Lucas-Rock-Clear <lucas.rock.clear@gmail.com>
2023-05-30 20:03:01 -0300

    finishing camera app

--- App.js
+++ App.js
@@ -1,14 +1,87 @@
 import React, { useState } from 'react';
-import {View, Text, StatusBar, StyleSheet, TouchableOpacity, Modal,} from 'react-native';
+import {View, Text, StatusBar, StyleSheet, TouchableOpacity, Modal, Image,
+   PermissionsAndroid, Platform } from 'react-native';
 import { RNCamera } from 'react-native-camera';
+import CameraRoll from '@react-native-community/cameraroll';
+import ImagePicker from 'react-native-image-picker';
 
 export default function App(){
   const [type, setType] = useState(RNCamera.Constants.Type.back);
   const [open, setOpen] = useState(false);
+  const [capturedPhoto, setCapturedPhoto] = useState(null);
 
-  function takePicture(){
+  async function takePicture(camera){
+    const options = { quality: 0.5, base64: true };
+    const data = await camera.takePictureAsync(options);
+
+    setCapturedPhoto(data.uri);
     setOpen(true);
+    console.log('FOTO TIRADA CAMERA: ' + data.uri);
+
+    //Chama funcao salvar a foto no album
+    savePicture(data.uri);
+
   }
+
+
+  async function hasAndroidPermission(){
+    const permission = PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE;
+
+    const hasPermission = await PermissionsAndroid.check(permission);
+    if(hasPermission){
+      return true;
+    }
+
+    const status = await PermissionsAndroid.request(permission);
+    return status === 'granted';
+  }
+
+  async function savePicture(data){
+    if(Platform.OS === 'android' && !(await hasAndroidPermission())){
+      return;
+    }
+
+    CameraRoll.save(data, 'photo')
+    .then((res) => {
+      console.log('SALVO COM SUCESSO: ' + res)
+    })
+    .catch((err)=>{
+      console.log('ERROR AO SALVAR: ' + err)
+    })
+
+
+  }
+
+  function toggleCam(){
+    setType(type === RNCamera.Constants.Type.back ? RNCamera.Constants.Type.front : RNCamera.Constants.Type.back)
+  }
+
+  function openAlbum(){
+    const options = {
+      title: 'Selecione uma foto',
+      chooseFromLibraryButtonTitle: 'Buscar foto do album..',
+      noData: true,
+      mediaType: 'photo'
+    };
+
+    ImagePicker.launchImageLibrary(options, (response) => {
+
+      if(response.didCancel){
+        console.log('Image Picker cancelado...');
+      }else if(response.error){
+        console.log('Gerou algum erro: ' + response.error);
+      }else{
+        setCapturedPhoto(response.uri);
+        setOpen(true);
+      }
+
+    })
+
+
+  }
+
+
+
 
   return(
     <View style={styles.container}>
@@ -31,34 +104,53 @@ export default function App(){
             <View 
             style={{marginBottom: 35, flexDirection: 'row', alignItems: 'flex-end', justifyContent: 'space-between'}}
             >
+
               <TouchableOpacity
-                onPress={()=> takePicture() }
+                onPress={()=> takePicture(camera) }
                 style={styles.capture}
               >
                 <Text>Tirar foto</Text>
               </TouchableOpacity>
+
               <TouchableOpacity
-                onPress={()=> {}}
+                onPress={openAlbum}
                 style={styles.capture}
               >
                 <Text>Album</Text>
               </TouchableOpacity>
+
             </View>
           );
         }}
       </RNCamera>
 
+      <View style={styles.camPosition}>
+        <TouchableOpacity onPress={toggleCam}>
+          <Text>Trocar</Text>
+        </TouchableOpacity>
+      </View>
 
-      <Modal animationType="slide" transparent={false} visible={open}>
-        <View style={{flex:1, justifyContent: 'center', alignItems: 'center', margin: 20}} >
-          <TouchableOpacity 
-          style={{margin: 10}}
-          onPress={() => setOpen(false)}
-          >
-            <Text style={{ fontSize: 24}}>Fechar</Text>
-          </TouchableOpacity>
-        </View>
-      </Modal>
+
+      { capturedPhoto && 
+        <Modal animationType="slide" transparent={false} visible={open}>
+          <View style={{flex:1, justifyContent: 'center', alignItems: 'center', margin: 20}} >
+            <TouchableOpacity 
+            style={{margin: 10}}
+            onPress={() => setOpen(false)}
+            >
+              <Text style={{ fontSize: 24}}>Fechar</Text>
+            </TouchableOpacity>
+
+            <Image
+              resizeMode="contain"
+              style={{width: 350, height: 450, borderRadius: 15}}
+              source={{ uri: capturedPhoto }}
+            />
+
+
+          </View>
+        </Modal>
+      }
 
 
     </View>
@@ -83,5 +175,14 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     alignSelf: 'center',
     margin: 20,
+  },
+  camPosition:{
+    backgroundColor: '#FFF',
+    borderRadius:5,
+    padding: 10,
+    height: 40,
+    position: 'absolute',
+    right: 25,
+    top: 60,
   }
 });
