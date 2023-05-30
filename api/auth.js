6d77622cb849e6d9c7979e397d7ab5eda770034e
Lucas-Rock-Clear <lucas.rock.clear@gmail.com>
2023-05-30 19:45:55 -0300

    basic task manager and auth on the proto user

---
+++ api/auth.js
@@ -0,0 +1,13 @@
+const { authSecret } = require('../.env')
+const jwt = require('jwt-simple')
+const bcrypt = require('bcrypt-nodejs')
+
+module.exports = app => {
+    const signin = async (req, res) => {
+        if (!req.body.email || !req.body.password) {
+            return res.status(400).send('Dados incompletos')
+        }
+
+
+    return { signin }
+}
