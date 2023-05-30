b57815af423c25c9bfb7e7a4502551a9e90470a2
Lucas-Rock-Clear <lucas.rock.clear@gmail.com>
2023-05-30 19:54:28 -0300

    Data registry CRUD, UX for the users and Homepage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,21 +1,66 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { View, Text, Button } from 'react-native';
+import firebase from '../../services/firebaseConnection';
+import { format } from 'date-fns';
 
 import { AuthContext } from '../../contexts/auth';
+import Header from '../../components/Header';
+import HistoricoList from '../../components/HistoricoList';
+
+import { Background, Container, Nome, Saldo, Title, List} from './styles';
 
 export default function Home() {
+  const [historico, setHistorico] = useState([]);
+  const [saldo, setSaldo] = useState(0);
 
-  const { user, signOut } = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
+  const uid = user && user.uid;
+
+  useEffect(()=>{
+    async function loadList(){
+      await firebase.database().ref('users').child(uid).on('value', (snapshot)=>{
+        setSaldo(snapshot.val().saldo);
+      });
+
+      await firebase.database().ref('historico')
+      .child(uid)
+      .orderByChild('date').equalTo(format(new Date, 'dd/MM/yy'))
+      .limitToLast(10).on('value', (snapshot)=>{
+        setHistorico([]);
+
+        snapshot.forEach((childItem) => {
+          let list = {
+            key: childItem.key,
+            tipo: childItem.val().tipo,
+            valor: childItem.val().valor
+          };
+          
+          setHistorico(oldArray => [...oldArray, list].reverse());
+        })
+      })
+
+    }
+
+    loadList();
+  }, []);
 
  return (
-   <View>
-       <Text>Home</Text>
-       <Text>{ user && user.nome }</Text>
-       <Text>{ user && user.email }</Text>
-       <Button
-       title="Sair da conta"
-       onPress={ () => signOut() }
-       />
-   </View>
+    <Background>
+      <Header/>
+      <Container>
+        <Nome>{user && user.nome}</Nome>
+        <Saldo>R$ {saldo.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.')}</Saldo>
+      </Container>
+
+      <Title>Ultimas movimentações</Title>
+
+      <List
+      showsVerticalScrollIndicator={false}
+      data={historico}
+      keyExtractor={ item => item.key}
+      renderItem={ ({ item }) => ( <HistoricoList data={item} /> )}
+      />
+
+    </Background>
   );
 }
