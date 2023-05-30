99ea6a8be47d4d3560f7d4002299abc73466e87f
Lucas-Rock-Clear <lucas.rock.clear@gmail.com>
2023-05-30 20:11:05 -0300

    redux, reducers, alt reducers and actions
    I had an inspired weekend

--- src/utils.js
+++
@@ -1,8 +0,0 @@
-import { Platform, PixelRatio} from 'react-native';
-
-export function getPixel(pixel){
-    return Platform.select({
-        ios: pixel,
-        android: PixelRatio.getPixelSizeForLayoutSize(pixel)
-    })
-}
